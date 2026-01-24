01ae53459023cabc997fd2a63bd73774ee1d0c2a
Zenowz <150805419+Zenowz@users.noreply.github.com>
2026-01-24 23:14:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-const toggleButton = document.getElementById('theme-toggle');
+const toggleButton = document.getElementById("theme-toggle");
 const body = document.body;
-toggleButton.addEventListener('click', () => {
-    body.classList.toggle('dark');
+
+toggleButton.addEventListener("click", () => {
+  body.classList.toggle("dark");
+});
+
+/* DROPDOWNS */
+const dropdowns = document.querySelectorAll(".dropdown-btn");
+
+dropdowns.forEach(button => {
+  button.addEventListener("click", () => {
+    const dropdown = button.parentElement;
+    dropdown.classList.toggle("active");
+  });
 });
